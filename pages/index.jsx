999107789248de41ff8c7d51238c18662236da02
Sam Selikoff <sam.selikoff@gmail.com>
2022-06-03 15:19:13 -0400

    End of video

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,67 +1,61 @@
 import { useState } from "react";
-import { AnimatePresence, motion } from "framer-motion";
 import * as Icons from "@heroicons/react/solid";
+import { AnimatePresence, motion } from "framer-motion";
 import useMeasure from "react-use-measure";
 
 export default function Home() {
-  let [count, setCount] = useState(1);
-  let previous = usePrevious(count);
   let [ref, { width }] = useMeasure();
-  let direction = count > previous ? 1 : -1;
+  let [count, setCount] = useState(1);
+  let prev = usePrevious(count);
+  let direction = count > prev ? 1 : -1;
 
   return (
-    <div className="mx-auto flex w-full  flex-col items-center py-16 px-4">
-      <div className="flex w-full justify-between">
-        <button
-          onClick={() => setCount(count - 1)}
-          className="flex h-8 w-8 items-center justify-center rounded-full bg-white hover:bg-gray-200"
-        >
+    <div className="text-white">
+      <div className="mt-4 flex justify-between px-4">
+        <button onClick={() => setCount(count - 1)}>
           <Icons.ChevronLeftIcon className="h-5 w-5" />
         </button>
-        <button
-          onClick={() => setCount(count + 1)}
-          className="flex h-8 w-8 items-center justify-center rounded-full bg-white hover:bg-gray-200"
-        >
+        <button onClick={() => setCount(count + 1)}>
           <Icons.ChevronRightIcon className="h-5 w-5" />
         </button>
       </div>
-      <div
-        ref={ref}
-        className="relative mt-8 flex h-28 w-1/2 items-center justify-center overflow-hidden bg-gray-700"
-      >
-        <AnimatePresence custom={{ direction, width }}>
-          <motion.div
-            key={count}
-            variants={variants}
-            initial="enter"
-            animate="center"
-            exit="exit"
-            custom={{ direction, width }}
-            className={`${
-              colors[Math.abs(count) % 4]
-            } absolute flex h-20 w-20 items-center justify-center text-xl font-bold text-white`}
-          >
-            {count}
-          </motion.div>
-        </AnimatePresence>
+      <div className="mt-8 flex justify-center">
+        <div
+          ref={ref}
+          className="relative flex h-24 w-1/2 items-center justify-center overflow-hidden bg-gray-700"
+        >
+          <AnimatePresence custom={{ direction, width }}>
+            <motion.div
+              key={count}
+              variants={variants}
+              initial="enter"
+              animate="center"
+              exit="exit"
+              custom={{ direction, width }}
+              className={`absolute flex h-20 w-20 items-center justify-center ${
+                colors[Math.abs(count) % 4]
+              }`}
+            >
+              {count}
+            </motion.div>
+          </AnimatePresence>
+        </div>
       </div>
     </div>
   );
 }
 
-let colors = ["bg-red-500", "bg-blue-500", "bg-green-500", "bg-yellow-500"];
 let variants = {
-  enter: ({ direction, width }) => ({
-    x: direction * width,
-  }),
+  enter: ({ direction, width }) => ({ x: direction * width }),
   center: { x: 0 },
-  exit: ({ direction, width }) => ({
-    x: direction * -width,
-  }),
+  exit: ({ direction, width }) => ({ x: direction * -width }),
 };
 
+let colors = ["bg-red-500", "bg-blue-500", "bg-green-500", "bg-yellow-500"];
+
 function usePrevious(state) {
-  let [tuple, setTuple] = useState([state, null]);
+  let [tuple, setTuple] = useState([null, state]);
+
   if (tuple[1] !== state) {
     setTuple([tuple[1], state]);
   }
